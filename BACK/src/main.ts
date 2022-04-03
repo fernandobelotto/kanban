71c339ec10adab5fa5b68cc2823e1a0681743296
Fernando Bosco <fbosco@dlocal.com>
2022-04-02 23:00:15 -0300

    update for new port with .env

--- BACK/src/main.ts
+++ BACK/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
-
+import { ConfigService } from "@nestjs/config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get("PORT");
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
@@ -14,7 +18,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-
-  await app.listen(5000);
+  await app.listen(port || 5000);
 }
 bootstrap();
